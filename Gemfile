baf7958a82a74f9699a94370096b6715781d3076
Sam Jeffress <Sam Jeffress>
2016-01-03 15:06:47 +1100

    added gemfile, rake, dummy test

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'rake'
+gem 'sqlite3'
+gem 'sequel'
---
+++ test/file_exists_test.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def test_that_kitty_can_eat
+    assert_equal "OHAI!", @meme.i_can_has_cheezburger?
+  end
+
+  def test_that_it_will_not_blend
+    refute_match /^no/i, @meme.will_it_blend?
+  end
+end
